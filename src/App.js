c71ffeec41a187e6666de5271a28096ec323aec7
Piotr Krzeminski <git@krzeminski.it>
2019-11-01 22:28:10 +0100

    WIP

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React, { useState } from 'react';
 import './App.css';
 import Container from '@material-ui/core/Container';
+import Divider from '@material-ui/core/Divider';
 import Paper from '@material-ui/core/Paper';
 import Slider from '@material-ui/core/Slider';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableRow from '@material-ui/core/TableRow';
 import Typography from '@material-ui/core/Typography'
 import { makeStyles } from '@material-ui/core/styles';
 import DotTriageComponent from './DotTriageComponent';
@@ -117,6 +122,46 @@ function App() {
             step={null}
             marks={impactMarks}
             onChange={(event, value) => setImpact(value)}/>
+          <Divider style={{margin: '20px 0'}}/>
+          <Typography gutterBottom>
+            Suggested order of priorities
+          </Typography>
+          <DotTriageComponent groupSizeAffected="small" probability="low" impact="low" size="40px"/>
+          <DotTriageComponent groupSizeAffected="medium" probability="low" impact="low" size="40px"/>
+          <DotTriageComponent groupSizeAffected="large" probability="low" impact="low" size="40px"/>
+          <DotTriageComponent groupSizeAffected="small" probability="medium" impact="low" size="40px"/>
+          <DotTriageComponent groupSizeAffected="medium" probability="medium" impact="low" size="40px"/>
+          <DotTriageComponent groupSizeAffected="large" probability="medium" impact="low" size="40px"/>
+          <DotTriageComponent groupSizeAffected="small" probability="high" impact="low" size="40px"/>
+          <DotTriageComponent groupSizeAffected="medium" probability="high" impact="low" size="40px"/>
+          <DotTriageComponent groupSizeAffected="large" probability="high" impact="low" size="40px"/>
+          <DotTriageComponent groupSizeAffected="small" probability="always" impact="low" size="40px"/>
+          <DotTriageComponent groupSizeAffected="medium" probability="always" impact="low" size="40px"/>
+          <DotTriageComponent groupSizeAffected="large" probability="always" impact="low" size="40px"/>
+          <DotTriageComponent groupSizeAffected="small" probability="low" impact="medium" size="40px"/>
+          <DotTriageComponent groupSizeAffected="medium" probability="low" impact="medium" size="40px"/>
+          <DotTriageComponent groupSizeAffected="large" probability="low" impact="medium" size="40px"/>
+          <DotTriageComponent groupSizeAffected="small" probability="medium" impact="medium" size="40px"/>
+          <DotTriageComponent groupSizeAffected="medium" probability="medium" impact="medium" size="40px"/>
+          <DotTriageComponent groupSizeAffected="large" probability="medium" impact="medium" size="40px"/>
+          <DotTriageComponent groupSizeAffected="small" probability="high" impact="medium" size="40px"/>
+          <DotTriageComponent groupSizeAffected="medium" probability="high" impact="medium" size="40px"/>
+          <DotTriageComponent groupSizeAffected="large" probability="high" impact="medium" size="40px"/>
+          <DotTriageComponent groupSizeAffected="small" probability="always" impact="medium" size="40px"/>
+          <DotTriageComponent groupSizeAffected="medium" probability="always" impact="medium" size="40px"/>
+          <DotTriageComponent groupSizeAffected="large" probability="always" impact="medium" size="40px"/>
+          <DotTriageComponent groupSizeAffected="small" probability="low" impact="high" size="40px"/>
+          <DotTriageComponent groupSizeAffected="medium" probability="low" impact="high" size="40px"/>
+          <DotTriageComponent groupSizeAffected="large" probability="low" impact="high" size="40px"/>
+          <DotTriageComponent groupSizeAffected="small" probability="medium" impact="high" size="40px"/>
+          <DotTriageComponent groupSizeAffected="medium" probability="medium" impact="high" size="40px"/>
+          <DotTriageComponent groupSizeAffected="large" probability="medium" impact="high" size="40px"/>
+          <DotTriageComponent groupSizeAffected="small" probability="high" impact="high" size="40px"/>
+          <DotTriageComponent groupSizeAffected="medium" probability="high" impact="high" size="40px"/>
+          <DotTriageComponent groupSizeAffected="large" probability="high" impact="high" size="40px"/>
+          <DotTriageComponent groupSizeAffected="small" probability="always" impact="high" size="40px"/>
+          <DotTriageComponent groupSizeAffected="medium" probability="always" impact="high" size="40px"/>
+          <DotTriageComponent groupSizeAffected="large" probability="always" impact="high" size="40px"/>
         </Paper>
       </Container>
     </div>
