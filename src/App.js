3398d6c6bc738a451e1e46103b847c52d968c392
Piotr Krzeminski <git@krzeminski.it>
2019-11-01 19:46:39 +0100

    Remove create-react-app content, add Material UI Paper background

--- src/App.js
+++ src/App.js
@@ -1,29 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Paper from '@material-ui/core/Paper';
+import { makeStyles } from '@material-ui/core/styles';
 import DotTriageComponent from './DotTriageComponent';
 
+const useStyles = makeStyles(theme => ({
+  root: {
+    padding: theme.spacing(3, 2),
+  },
+}));
+
 function App() {
+  const classes = useStyles();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <Paper className={classes.root}>
         <DotTriageComponent
             groupSizeAffected="medium"
             probability="high"
             impact="low"/>
-      </header>
+      </Paper>
     </div>
   );
 }
