b607efb119b2048fa694757646db4ddcadf02d0e
Piotr Krzeminski <git@krzeminski.it>
2019-11-01 20:06:25 +0100

    Add three sliders

--- src/App.js
+++ src/App.js
@@ -1,14 +1,65 @@
 import React from 'react';
 import './App.css';
 import Paper from '@material-ui/core/Paper';
+import Slider from '@material-ui/core/Slider';
+import Typography from '@material-ui/core/Typography'
 import { makeStyles } from '@material-ui/core/styles';
 import DotTriageComponent from './DotTriageComponent';
 
 const useStyles = makeStyles(theme => ({
   root: {
-    padding: theme.spacing(3, 2),
+    padding: theme.spacing(5, 5),
   },
 }));
+
+const groupSizeAffectedMarks = [
+  {
+    value: 1,
+    label: 'small',
+  },
+  {
+    value: 2,
+    label: 'medium',
+  },
+  {
+    value: 3,
+    label: 'high',
+  },
+];
+
+const probabilityMarks = [
+  {
+    value: 1,
+    label: 'low',
+  },
+  {
+    value: 2,
+    label: 'medium',
+  },
+  {
+    value: 3,
+    label: 'high',
+  },
+  {
+    value: 4,
+    label: 'always',
+  },
+];
+
+const impactMarks = [
+  {
+    value: 1,
+    label: 'low',
+  },
+  {
+    value: 2,
+    label: 'medium',
+  },
+  {
+    value: 3,
+    label: 'high',
+  },
+];
 
 function App() {
   const classes = useStyles();
@@ -20,6 +71,36 @@ function App() {
             groupSizeAffected="medium"
             probability="high"
             impact="low"/>
+        <Typography gutterBottom>
+          Group size affected
+        </Typography>
+        <Slider
+          min={1}
+          max={3}
+          defaultValue={2}
+          aria-labelledby="discrete-slider-restrict"
+          step={null}
+          marks={groupSizeAffectedMarks}/>
+        <Typography gutterBottom>
+          Probability
+        </Typography>
+        <Slider
+          min={1}
+          max={4}
+          defaultValue={3}
+          aria-labelledby="discrete-slider-restrict"
+          step={null}
+          marks={probabilityMarks}/>
+        <Typography gutterBottom>
+          Impact
+        </Typography>
+        <Slider
+          min={1}
+          max={3}
+          defaultValue={2}
+          aria-labelledby="discrete-slider-restrict"
+          step={null}
+          marks={impactMarks}/>
       </Paper>
     </div>
   );
