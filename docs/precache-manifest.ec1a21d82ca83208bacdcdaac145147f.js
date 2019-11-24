24016865f56b225d25f26c065729dfcc96f9300e
Piotr Krzeminski <git@krzeminski.it>
2019-11-24 12:25:44 +0100

    Add production build to docs/

---
+++ docs/precache-manifest.ec1a21d82ca83208bacdcdaac145147f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9452268f271e2e55d5f22c27229ee0d5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ece498ed87a27435ad9",
+    "url": "/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "06383566c103a548b4aa",
+    "url": "/static/js/2.f7268ca9.chunk.js"
+  },
+  {
+    "revision": "4ece498ed87a27435ad9",
+    "url": "/static/js/main.fe547798.chunk.js"
+  },
+  {
+    "revision": "6842248c7d74088baa39",
+    "url": "/static/js/runtime-main.57bf19f9.js"
+  }
+]);
